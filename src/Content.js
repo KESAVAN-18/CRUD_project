26100d3778f53dae835fd06718b93db611e54c52
REOpranav <pranavavadivel01@gmail.com>
2023-07-15 23:30:45 +0530

    adding content

---
+++ src/Content.js
@@ -0,0 +1,30 @@
+
+
+const Content = ({input,handleChange,handleDelete}) => {
+
+    return (
+     
+        <>
+             {input.length?(
+              <ul>
+                {input.map((item)=>{
+                  return  <li key={item.id} className="list ">
+                      <label> <input type="checkbox" 
+                                 checked =  {item.checked}
+                                 onChange={()=>handleChange(item.id)}                         
+                          />
+                              {item.productName}</label>
+                          <button 
+                          
+                          role='button'
+                          onClick={()=>handleDelete(item.id)}
+                          >🅾</button>
+                    </li>
+                })}
+              </ul> ): <p style={{fontFamily:"monospace",opacity :"50%"}}>your list are empty</p> }
+        </>
+   
+    )
+  }
+  
+  export default Content
