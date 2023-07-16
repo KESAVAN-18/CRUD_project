3bde08a1d66b512ab00c68d54fb751b18cb8335c
REOpranav <pranavavadivel01@gmail.com>
2023-07-16 16:51:35 +0530

    inserting color changing state

--- src/AddItems.js
+++ src/AddItems.js
@@ -2,23 +2,26 @@
 const AddItems = ({handleSubmit,additem,setAdditem,returnFocus}) => {
    
     return (
-     <div>
-        <form onSubmit={handleSubmit} className="inputbox">
+     <div className="additems">
+        <form onSubmit={handleSubmit}>
                       <input type="text"       
-                            autoFocus
-                            ref={returnFocus}
-                            placeholder='enter a list '
-                            value = {additem}
-                            onChange={(e)=>setAdditem(e.target.value)}
+                              autoFocus
+                              ref={returnFocus}
+                              placeholder='enter a list '
+                              value = {additem}
+                              onChange={(e)=>setAdditem(e.target.value)}
                         
                       />
   
                       <button 
+                         className="send"
                          role="submit"
                          onClick={()=>returnFocus.current.focus()} 
-                      >✅</button>
-  
+                      >submit</button>
+
+
             </form>
+    
      </div>
     )
   }
