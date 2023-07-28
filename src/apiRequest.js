eb4545d4b0f8bc4df0ba890a0ece950f64c25115
REOpranav <pranavavadivel01@gmail.com>
2023-07-28 21:19:25 +0530

    Normal updates

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,4 +1,4 @@
-const apiRequest = async(url = '', optionalObj = null , errMsg = null)=>{
+const apiRequest = async (url = '', optionalObj = null , errMsg = null)=>{
     try {
         const responce = await fetch (url,optionalObj)
         if(!responce.ok) throw Error ('PLSE RELOAD THE APP')
